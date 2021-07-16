5930995679e16d6d37c126298c8fdc3a25d966f8
luis-blash <blash3g@gmail.com>
2021-07-16 13:50:29 -0500

    Socket io implementado

--- models/server.js
+++ models/server.js
@@ -1,32 +1,49 @@
 const express = require('express')
 const cors = require('cors')
 
-class Server{
-    constructor(){
+class Server {
+    constructor() {
         this.app = express()
         this.port = process.env.PORT
+        // Server
+        this.server = require('http').createServer(this.app)
+        this.io = require('socket.io')(this.server);
+
         // Rutas
         this.pathRoute = {
-            home: '/home'
         }
         //middlewares
         this.middlewares()
         // routes
         this.routes()
 
+        // socket
+        this.sockets()
+
     }
 
-    middlewares(){
+    middlewares() {
         this.app.use(cors());
         this.app.use(express.static('public'));
     }
 
-    routes(){
-        this.app.use(this.pathRoute.home, require('../routes/home'))
+    routes() {
+        // this.app.use(this.pathRoute.home, require('../routes/home'))
     }
 
-    listen(){
-        this.app.listen(this.port, () =>{console.log(`Port ${this.port}`);})
+    sockets(){
+        this.io.on('connection', socket => {
+            console.log('Cliente conectado', socket.id);
+
+            socket.on('disconnect', ()=>{
+                console.log('Cliente desconectado');
+            })
+        });
+    }
+
+    listen() {
+        // this.app.listen(this.port, () => { console.log(`Port ${this.port}`); })
+        this.server.listen(this.port, () => { console.log(`Server Port ${this.port}`); })
     }
 }
 
